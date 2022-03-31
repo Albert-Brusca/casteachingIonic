b02758fa64eecf113592b8a61f4681fc4f856035
Albert-Brusca <abrusca@iesebre.com>
2022-03-31 19:20:58 +0200

    Refresh del nom del usuari

--- src/views/User.vue
+++ src/views/User.vue
@@ -19,6 +19,8 @@
         <h1>User</h1>
         name: {{ this.user && this.user.name}}
       </div>
+      <ion-button v-if="disabled===false" @click="refresh">Refresh</ion-button>
+      <ion-spinner v-else-if="disabled === true" name="bubbles"></ion-spinner>
     </ion-content>
   </ion-page>
 </template>
@@ -30,6 +32,9 @@ import {
   IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar,
 } from "@ionic/vue";
 import store from "../store"
+// import axios from "axios";
+// import adapter from 'axios/lib/adapters/xhr'
+
 
 export default {
   name: "user",
@@ -44,11 +49,50 @@ export default {
   },
   data () {
     return {
-      user: {}
+      user: {},
+      token: "",
+      disabled: false
     }
   },
   async mounted() {
     this.user = await store.get('user')
+    this.token = await store.get('token')
+  },
+  methods: {
+    async refresh() {
+      this.disabled = true
+
+      // console.log(this.token);
+      // const axiosClient = axios.create({
+      //   adapter,
+      //   baseURL: 'https://casteaching.albertbrusca.me/api',
+      //   withCredentials: true,
+      //   headers: {
+      //     Accept: 'application/json',
+      //     'Content-Type': 'application/json',
+      //     Authorization: 'Bearer ' + this.token
+      //   }
+      // })
+      // let response = [];
+      // try {
+      //   response = await axiosClient.get('/user')
+      //   await store.set('user', response.data)
+      //   console.log(this.user)
+      // } catch (error) {
+      //   console.log(error);
+      // }
+
+      let user
+      try {
+        user = await this.casteaching.user()
+      } catch (error) {
+        console.log(error);
+      }
+      await store.set('user', user)
+
+      await window.location.reload()
+      this.disabled=false
+    }
   }
 }
 </script>
